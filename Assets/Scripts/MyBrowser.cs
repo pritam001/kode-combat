d7383fa6bb849eb5cd11bcb8e317d0332cb59675
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-11 13:56:08 +0530

    Game v1

---
+++ Assets/Scripts/MyBrowser.cs
@@ -0,0 +1,268 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+ 
+
+public class MyBrowser : MonoBehaviour {
+	
+	private string curr = Directory.GetCurrentDirectory();
+	private string filename = null;
+	private int selectionGridInt = -1 ;
+	private Vector2 scrollViewVector = Vector2.zero;
+	public Texture2D ControlTexture1;
+	public Texture2D ControlTexture2;
+	public Texture2D WindowBackgroundTex;
+	public Texture2D tickTex;
+	public Texture2D crossTex;
+	private Rect modalRect = new Rect(100, 100, Screen.width - 200, Screen.height - 200);
+	private bool showModal = false;
+	Texture2D tex = null;
+	
+	
+	void Awake() {
+        DontDestroyOnLoad(tex);
+    }
+	
+	void OnGUI () {
+        // Make a background box
+        GUI.Box(new Rect(10,10,Screen.width - 20 ,Screen.height - 20), "<b><color=yellow>File Browser</color></b>");
+		GUIStyle gsSelGrid = new GUIStyle(GUI.skin.button); //Copy the Default style for buttons
+		//gsSelGrid.fontSize = 9; //Change the Font size
+		GUI.skin.button.fixedHeight = 30; // make button size for all buttons fixed
+		gsSelGrid.richText = true;
+		
+		
+		// Moving up the directory
+		if(GUI.Button(new Rect(0,0,30,30), new GUIContent(ControlTexture1,"Go up the directory") )){
+			print(Directory.GetParent(curr).FullName);
+			curr = Directory.GetParent(curr).FullName;
+			selectionGridInt = -1;
+			//goto skip;
+		}
+
+		
+		if(GUI.Button(new Rect(Screen.width - 30,0,30,30), new GUIContent(ControlTexture2,"Open file browser") )){
+			print("Open explorer");
+			OpenInFileBrowser.Open(curr);
+		}
+		
+		GUI.Label(new Rect(Screen.width - 200,40,200,20), GUI.tooltip);
+		
+		GUI.Label(new Rect(40,30,Screen.width - 80,30), curr);
+		string[] dir = Directory.GetDirectories(curr);
+		int i = 0;
+		foreach(string temp in dir){
+			dir[i] = (Path.GetFileName(temp));
+			i = i + 1; 
+		}
+		int numberOfFolders = i - 1;
+
+		//filter all image types
+		var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+		var files = GetFilesFrom( curr, filters, false);
+		//string[] files = Directory.GetFiles(curr, "*.png");
+		i = 0;
+		foreach(string temp in files){
+			files[i] = (Path.GetFileName(temp));
+			i = i + 1; 
+		}
+
+		string[] all = dir.Concat(files).ToArray();
+		
+		scrollViewVector = GUI.BeginScrollView(new Rect(40,60,Screen.width - 80,Screen.height - 100),scrollViewVector,new Rect(0,0,Screen.width,12*(numberOfFolders + i)));
+		selectionGridInt = GUI.SelectionGrid(new Rect(0,0,Screen.width, 40*(numberOfFolders + i) ),selectionGridInt, all, 3,gsSelGrid);
+		GUI.EndScrollView();
+		
+		if(GUI.changed && selectionGridInt != -1 && selectionGridInt <= numberOfFolders){
+			print(selectionGridInt);
+			curr = curr + "\\" + dir[selectionGridInt];
+			selectionGridInt = -1;
+		} else if(GUI.changed && selectionGridInt != -1 && selectionGridInt > numberOfFolders){
+			print("Image is selected. " + selectionGridInt);
+			filename = curr + "\\" + all[selectionGridInt];
+			showModal = true;
+		}
+		
+		if(showModal){
+			modalRect = GUI.ModalWindow(0, modalRect, ImageModalWindowFunction, "Image Preview");
+		}
+		
+    }
+	
+	void ImageModalWindowFunction(int windowID){
+		GUIStyle centeredStyle = new GUIStyle(GUI.skin.label);
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		
+		GUI.DrawTexture(new Rect(-100,-100,Screen.width, Screen.height ), WindowBackgroundTex, ScaleMode.ScaleToFit, true, 1.0F);
+		GUI.Label(new Rect(Screen.width/2 - 150, 10, 200, 30),"<color=blue><b> Image Preview : </b></color>");
+		GUI.Box(new Rect(0, 0, Screen.width - 200, 40), "");
+		
+		
+		byte[] fileData = File.ReadAllBytes(filename);
+        tex = new Texture2D(2, 2);
+        tex.LoadImage(fileData);
+		
+		GUI.Label(new Rect(20,40,Screen.width - 200,Screen.height - 300), new GUIContent(tex), centeredStyle);
+		
+		if (GUI.Button(new Rect(Screen.width/2 - 250, Screen.height - 250, 100, 20), new GUIContent("Select",tickTex))){
+			print("Import to next scene");
+			PlayerPrefs.SetString("filename", filename);
+			Application.LoadLevel ("loadnplay"); 
+		}
+		if (GUI.Button(new Rect(Screen.width/2 - 50, Screen.height - 250, 100, 20), new GUIContent("Close",crossTex))){
+			fileData = null;
+			Resources.UnloadUnusedAssets();  //release the memory of previous texture loaded
+			System.GC.Collect();
+			selectionGridInt = -1;
+			showModal =false;
+		}
+	}
+	
+	
+	// Get certain types of file. Use GetFilesFrom(searchFolder, filters, false);
+	public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive){
+		List<String> filesFound = new List<String>();
+		var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+		foreach (var filter in filters)
+		{
+		   filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+		}
+		return filesFound.ToArray();
+	}
+	
+	//Use `OpenInFileBrowser.Open()` for a cross-platform way of opening any file/folder.
+	public static class OpenInFileBrowser
+	{
+		public static bool IsInMacOS
+		{
+			get
+			{
+				return UnityEngine.SystemInfo.operatingSystem.IndexOf("Mac OS") != -1;
+			}
+		}
+	 
+		public static bool IsInWinOS
+		{
+			get
+			{
+				return UnityEngine.SystemInfo.operatingSystem.IndexOf("Windows") != -1;
+			}
+		}
+	 
+		[UnityEditor.MenuItem("Window/Test OpenInFileBrowser")]
+		public static void Test()
+		{
+			Open(UnityEngine.Application.dataPath);
+		}
+	 
+		public static void OpenInMac(string path)
+		{
+			bool openInsidesOfFolder = false;
+	 
+			// try mac
+			string macPath = path.Replace("\\", "/"); // mac finder doesn't like backward slashes
+	 
+			if ( System.IO.Directory.Exists(macPath) ) // if path requested is a folder, automatically open insides of that folder
+			{
+				openInsidesOfFolder = true;
+			}
+	 
+			if ( !macPath.StartsWith("\"") )
+			{
+				macPath = "\"" + macPath;
+			}
+	 
+			if ( !macPath.EndsWith("\"") )
+			{
+				macPath = macPath + "\"";
+			}
+	 
+			string arguments = (openInsidesOfFolder ? "" : "-R ") + macPath;
+	 
+			try
+			{
+				System.Diagnostics.Process.Start("open", arguments);
+			}
+			catch ( System.ComponentModel.Win32Exception e )
+			{
+				// tried to open mac finder in windows
+				// just silently skip error
+				// we currently have no platform define for the current OS we are in, so we resort to this
+				e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+			}
+		}
+	 
+		public static void OpenInWin(string path)
+		{
+			bool openInsidesOfFolder = false;
+	 
+			// try windows
+			string winPath = path.Replace("/", "\\"); // windows explorer doesn't like forward slashes
+	 
+			if ( System.IO.Directory.Exists(winPath) ) // if path requested is a folder, automatically open insides of that folder
+			{
+				openInsidesOfFolder = true;
+			}
+	 
+			try
+			{
+				System.Diagnostics.Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
+			}
+			catch ( System.ComponentModel.Win32Exception e )
+			{
+				// tried to open win explorer in mac
+				// just silently skip error
+				// we currently have no platform define for the current OS we are in, so we resort to this
+				e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+			}
+		}
+	 
+		public static void Open(string path)
+		{
+			if ( IsInWinOS )
+			{
+				OpenInWin(path);
+			}
+			else if ( IsInMacOS )
+			{
+				OpenInMac(path);
+			}
+			else // couldn't determine OS
+			{
+				OpenInWin(path);
+				OpenInMac(path);
+			}
+		}
+	}
+	
+	/*private string url = null;
+	private Texture2D newTex = null;
+	IEnumerator Loadimage() {
+		print("ok");
+		url = "file:///" + getfilename(filename);
+        WWW www = new WWW(url);
+        yield return www;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+		newTex = www.texture;
+    }
+	
+	public string getfilename(string filename){
+		string temp = filename.Replace(@"\",@"/");
+		print(url);
+		return filename;
+	}*/
+	
+	
+	void Start(){
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/loader.cs
@@ -0,0 +1,299 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class loader : MonoBehaviour {
+	
+	public class piece{
+		public GameObject GameObject { get; set; }
+		public Vector3 Original { get; set; }
+		public Vector3 Current { get; set; }
+	}
+	public piece[,] Matrix = new piece[20, 1]; // piece[MaxRow, MaxColumn]
+	
+	private Texture2D tex;  // Texture to cut up
+	public Material mat;   // Material to use (texture is discarded)
+	public int cols = 1;   // Number of tiles across
+	public float aspect = 5f;  // Original aspect (height / width)
+	public int steps = 0;	// Total steps taken
+	public bool gameOn = false; // Whether game has started or not
+	public bool gameEnded = false; // Whether game is finished or not
+	public float timeLeft = 20f;	// Time left before game ends
+	public int score = 0;	// Score made
+	private float countDown = 3f;	// Count Down hits
+	public bool gameWon = false;
+	
+	private Vector3 pos1 = Vector3.zero; // latest selected object's position 
+	private Vector3 pos2 = Vector3.zero; // last selected object's position 
+	private RaycastHit hit1, hit2, hit_latest;
+	
+	// GUI Elements
+	private GUIStyle boxStyle1 = null;
+	public int maxHealth = 100;
+	public int curHealth = 100;
+	public Texture2D bgImage; 
+	public Texture2D fgImage; 
+	public float healthBarLength;
+	
+	void OnGUI(){
+		if(!gameOn && !gameEnded){
+			InitStyles();
+			// Make a background box
+			GUI.Box(new Rect(10,10,Screen.width - 20 ,Screen.height - 20), "<b><color=yellow><size=30>GAME STARTS IN</size></color></b>", boxStyle1);
+			GUI.Label(new Rect(Screen.width/2 - 30,Screen.height/2 - 30,60,60), "<b><color=red><size=50>"+Mathf.RoundToInt(countDown).ToString()+"</size></color></b>");
+		}
+		
+		if(gameOn && !gameEnded){
+			// Create one Group to contain both images
+			// Adjust the first 2 coordinates to place it somewhere else on-screen
+			GUI.BeginGroup (new Rect (20,20, healthBarLength,32));
+
+			// Draw the background image
+			GUI.Box (new Rect (0,0, healthBarLength,32), bgImage);
+
+			// Create a second Group which will be clipped
+			// We want to clip the image and not scale it, which is why we need the second Group
+			GUI.BeginGroup (new Rect (0,0, healthBarLength, 32));
+
+			// Draw the foreground image
+			GUI.Box (new Rect (2,2,curHealth / maxHealth * healthBarLength - 4,28), "", boxStyle1);
+
+			// End both Groups
+			GUI.EndGroup ();
+
+			GUI.EndGroup ();
+			
+			GUI.Box(new Rect(30 ,Screen.height - 40, Screen.width/2 - 60,30), "<b><color=yellow><size=20>Time Left: " + Mathf.RoundToInt(timeLeft).ToString() + "</size></color></b>", boxStyle1);
+			GUI.Box(new Rect(Screen.width/2 + 30 ,Screen.height - 40, Screen.width/2 - 60,30), "<b><color=yellow><size=20>Steps Taken : " + steps.ToString() + "</size></color></b>", boxStyle1);
+		}
+		
+		if(!gameOn && gameEnded){
+			// Make a background box
+			if(gameWon == true ){
+				GUI.Box(new Rect(10,10,Screen.width - 20 ,Screen.height - 20), "<b><color=yellow><size=30>YOU WON THE CHALLENGE!</size></color></b>", boxStyle1);
+			} else {
+				GUI.Box(new Rect(10,10,Screen.width - 20 ,Screen.height - 20), "<b><color=yellow><size=30>YOU LOST THE CHALLENGE!</size></color></b>", boxStyle1);
+			}
+			if(GUI.Button(new Rect(Screen.width/2 - 100,Screen.height/2, 200 ,50),"<b><color=white><size=15>Restart</size></color></b>")){
+				Application.LoadLevel("loadnplay");
+			}
+			if(GUI.Button(new Rect(Screen.width/2 - 100,Screen.height/2 + 60, 200 ,50),"<b><color=white><size=15>Go to Main Menu</size></color></b>")){
+				//Application.LoadLevel("loadnplay");
+			}
+		}
+	}
+	
+	public void AdjustCurrentHealth(int adj){
+ 
+		curHealth += adj;
+ 
+        if(curHealth <0)
+          curHealth = 0;
+ 
+        if(curHealth > maxHealth)
+          curHealth = maxHealth;
+ 
+        if(maxHealth <1)
+          maxHealth = 1;
+ 
+        healthBarLength =(Screen.width - 40) * (curHealth / (float)maxHealth);
+     }
+	
+	private void InitStyles()
+	{
+		if( boxStyle1 == null )
+		{
+			boxStyle1 = new GUIStyle( GUI.skin.box );
+			boxStyle1.normal.background = MakeTex( 2, 2, new Color( 1f, 0f, 0f, 0.25f ) );
+		}
+	}
+	 
+	private Texture2D MakeTex( int width, int height, Color col )
+	{
+		Color[] pix = new Color[width * height];
+		for( int i = 0; i < pix.Length; ++i )
+		{
+			pix[ i ] = col;
+		}
+		Texture2D result = new Texture2D( width, height );
+		result.SetPixels( pix );
+		result.Apply();
+		return result;
+	}
+	
+	
+	
+	// Start Function
+	void Start() {
+		bgImage = MakeTex(2,2, new Color( 1f, 0f, 0f, 1f ));
+		fgImage = MakeTex(2,2, new Color( 0f, 0f, 0f, 1f ));
+		healthBarLength = Screen.width - 40;
+		Resources.UnloadUnusedAssets();  //release the memory of previous texture loaded
+		string filepath = PlayerPrefs.GetString("filename");
+		byte[] fileData = File.ReadAllBytes(filepath);
+        tex = new Texture2D(2, 2);
+        tex.LoadImage(fileData);
+		mat.mainTexture = tex;
+		BuildPieces();
+		randomize();
+	}
+ 
+	void BuildPieces() {
+		int rows = Mathf.RoundToInt(cols * aspect);
+		Vector3 offset = Vector3.zero;
+		offset.x = 0f;
+		offset.y = -1.5f;
+		float startX = offset.x;
+		float uvWidth = 1.0f / cols;
+		float uvHeight = 1.0f / rows;
+
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				GameObject go = GameObject.CreatePrimitive (PrimitiveType.Quad);
+				Transform t = go.transform;
+				t.position = offset;
+				t.localScale = new Vector3(3f, 3*cols*0.99f/rows, 1f);
+				go.GetComponent<Renderer>().material = mat;
+				
+				Matrix[i,j] = new piece();
+				Matrix[i,j].GameObject = go;
+				Matrix[i,j].Original = offset;
+				Matrix[i,j].Current = offset;
+
+				Mesh mesh = go.GetComponent<MeshFilter>().mesh;
+				Vector2[] uvs = mesh.uv;
+				uvs[0] = new Vector2(j * uvWidth, i * uvHeight);
+				uvs[3] = new Vector2(j * uvWidth, (i + 1) * uvHeight);
+				uvs[1] = new Vector2((j + 1) * uvWidth, (i + 1) * uvHeight);
+				uvs[2] = new Vector2((j + 1) * uvWidth, i * uvHeight);
+				mesh.uv = uvs;
+				offset.x += 1f;
+			}
+			offset.y += 3*cols*1f/rows + 0.01f;
+			offset.x = startX;
+		}
+	}
+	
+	void randomize(){
+		start_random:
+		int perfect_random = 0;
+		for (int i = 0; i < cols*aspect; i++){
+			int random_i = UnityEngine.Random.Range(0, (int)aspect*cols);
+			if(Matrix[i,0].Current == Matrix[i,0].Original){
+				MatrixSwap(i,random_i);
+			} else{
+				perfect_random += 1;
+			}
+		}
+		if(perfect_random < cols*aspect){
+			goto start_random;
+		}
+		
+	}
+	
+	void MatrixSwap(int i, int j){
+		Vector3 temp1 = Matrix[i,0].GameObject.transform.position;
+		Vector3 temp2 = Matrix[j,0].GameObject.transform.position;
+		Matrix[i,0].GameObject.transform.position = temp2;
+		Matrix[i,0].Current = temp2;
+		Matrix[j,0].GameObject.transform.position = temp1;
+		Matrix[j,0].Current = temp1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Before game starts
+		if(gameOn == false && gameEnded == false){
+			countDown -= Time.deltaTime;
+			if(countDown < 0){
+				gameOn = true;
+			}
+		} else if (gameOn == true && gameEnded == false){ 
+			// Decrease time counter
+			timeLeft -= Time.deltaTime;
+			// If time is over, player loses
+			if(timeLeft < 0){
+				gameOn = false;
+				gameEnded = true;
+			}
+			
+			//Modify score and bar
+			AdjustCurrentHealth(1);
+			
+			if (Input.GetButtonDown("Fire1")) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if(Physics.Raycast(ray,out hit_latest,Mathf.Infinity)){
+					//Destroy(hit1.transform.gameObject);
+					// if 1st click in game, initialize RaycastHit objects and Vector3 positions
+					if(pos2 == Vector3.zero){
+						hit1 = hit2 = hit_latest;
+						pos1 = pos2 = hit_latest.transform.position;
+					} else {
+						hit2 = hit1;
+						hit1 = hit_latest;
+						pos2 = pos1;
+						pos1 = hit_latest.transform.position;
+					}
+					
+					if(pos1 != pos2){
+						swap();
+						steps += 1;
+					}
+					
+				}
+			}
+			// check if game has been won
+			CheckForVictory();
+		} else if (gameOn == false && gameEnded == true){ 
+		
+		}
+	}
+	
+	int CheckForVictory(){
+		int matched = 0;
+		for (int i = 0; i < cols*aspect; i++){
+			try{
+				if(Matrix[i,0].Current == Matrix[i,0].Original){
+					matched += 1;
+				}
+			} catch(Exception exception)
+			{
+			   Debug.Log(exception.ToString() + i.ToString()); // Null Reference Exception appears for unknown reasons.
+			}
+		}
+
+		if(matched == cols*aspect){
+			gameOn = false;
+			gameEnded = true;
+			gameWon = true;
+			return 1;
+		}
+		return 0;
+	}
+	
+	void swap(){
+		hit2.transform.position = pos1;
+		hit1.transform.position = pos2;
+		
+		// Modify current position of Matrix class after swap
+		for(int temp = 0; temp < cols*aspect; temp++){
+			try{
+				if(Matrix[temp,0].Current == pos1){
+					Matrix[temp,0].Current = pos2;
+				} else if(Matrix[temp,0].Current == pos2){
+					Matrix[temp,0].Current = pos1;
+				} 
+			} catch(Exception exception)
+			{
+			   Debug.Log(exception); // Null Reference Exception appears for unknown reasons.
+			}
+		}
+		
+		// re initialize
+		pos1 = pos2 = Vector3.zero;
+		hit_latest = hit1 = hit2;
+	}
+}
