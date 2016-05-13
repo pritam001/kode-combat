3260be6ff826157992be57d277feccb64c465513
Pritam Sarkar <001pritam2012@gmail.com>
2016-05-13 15:47:01 +0530

    Added Settings Tab and Game Settings

--- Assets/Scripts/ObjectLabel.cs
+++ Assets/Scripts/ObjectLabel.cs
@@ -6,13 +6,6 @@ public class ObjectLabel : MonoBehaviour {
  
 	public Transform target;  // Object that this label should follow
 	public GameObject loaderObject; // Object that contains loader.cs
-	/*public Vector3 offset = Vector3.up;    // Units in world space to offset; 1 unit above object by default
-	public bool clampToScreen = false;  // If true, label will be visible even if object is off screen
-	public float clampBorderSize = 0.05f;  // How much viewport space to leave at the borders when a label is being clamped
-	public bool useMainCamera = true;   // Use the camera tagged MainCamera
-	public Camera cameraToUse ;   // Only use this if useMainCamera is false
-	Camera cam ;
-	Transform camTransform; */
 	Transform thisTransform;
 	float obj_curr_y;
 	float target_curr_y;
@@ -25,49 +18,43 @@ public class ObjectLabel : MonoBehaviour {
 		target_curr_y = target.position.y;
 		loaderScript = loaderObject.GetComponent<loader>();
 		
-		/*if (useMainCamera)
-			cam = Camera.main;
-		else
-			cam = cameraToUse;
-		camTransform = cam.transform;*/
+		// Get start location after randomization
+		position_initialize();
 	}
  
+	void position_initialize() {
+		// Determine the slot number (0,1,2,....) target is in
+		int loc = Mathf.RoundToInt((target.position.y + 1.5f)/(3*loaderScript.cols*1f/Mathf.RoundToInt(loaderScript.cols * loaderScript.aspect) + 0.01f));
+		Debug.Log(loc.ToString() + "  " + transform.position.y.ToString());
+		
+		Vector3 newPosition = transform.position;
+		newPosition.y = 440 - loc*loaderScript.spaceBtwnLines;
+		Debug.Log(loc.ToString() + "  " + newPosition.y.ToString());
+		transform.position = newPosition;
+	}
  
 	void Update()
 	{
-
-		/*if (clampToScreen)
-		{
-			Vector3 relativePosition = camTransform.InverseTransformPoint(target.position + offset);
-			relativePosition.z =  Mathf.Max(relativePosition.z, 1.0f);
-			thisTransform.position = cam.WorldToViewportPoint(camTransform.TransformPoint(relativePosition));
-			thisTransform.position = new Vector3(Mathf.Clamp(thisTransform.position.x, clampBorderSize, 1.0f - clampBorderSize),
-											 Mathf.Clamp(thisTransform.position.y, clampBorderSize, 1.0f - clampBorderSize),
-											 thisTransform.position.z);
-
-		}
-		else
-		{
-			thisTransform.position = cam.WorldToViewportPoint(target.position + offset);
-		}*/
 		if(target.position.y < target_curr_y){
 			//Moving down
+			Debug.Log("Moving down " + ((target.position.y - target_curr_y)/(3*loaderScript.cols*1f/Mathf.RoundToInt(loaderScript.cols * loaderScript.aspect) + 0.01f)).ToString());
 			obj_curr_y -= loaderScript.spaceBtwnLines*(target.position.y - target_curr_y)/(3*loaderScript.cols*1f/Mathf.RoundToInt(loaderScript.cols * loaderScript.aspect) + 0.01f);
 			Vector3 newPosition = transform.position;
 			newPosition.y = obj_curr_y;
 			transform.position = newPosition;
 			
 			target_curr_y = target.position.y;
-			Debug.Log("Moving down");
+			
 		} else if(target.position.y > target_curr_y) {
 			//Moving up
+			Debug.Log("Moving up " + ((target.position.y - target_curr_y)/(3*loaderScript.cols*1f/Mathf.RoundToInt(loaderScript.cols * loaderScript.aspect) + 0.01f)).ToString());
 			obj_curr_y += loaderScript.spaceBtwnLines*(target.position.y - target_curr_y)/(3*loaderScript.cols*1f/Mathf.RoundToInt(loaderScript.cols * loaderScript.aspect) + 0.01f);
 			Vector3 newPosition = transform.position;
 			newPosition.y = obj_curr_y;
 			transform.position = newPosition;
 			
 			target_curr_y = target.position.y;
-			Debug.Log("Moving up");
+			
 		}
 	}
 }
